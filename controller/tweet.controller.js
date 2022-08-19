1b125a8a6bb64022e73b2b66f55269eecbaccabc
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-08-19 20:06:09 +0530

    adding post, Tweets, feed, like

--- controller/tweet.controller.js
+++ controller/tweet.controller.js
@@ -1,7 +1,7 @@
 const db = require('../model');
 const Tweet = db.tweet;
 const User = db.user;
-
+const Like = db.like;
 exports.post = async (req, res) =>{
     
     var username;
@@ -38,12 +38,13 @@ exports.post = async (req, res) =>{
 exports.Tweets = async (req, res) =>{
     
     Tweet.findAll({
-        include : [{
-            model : User,
-            where : {
-                id : req.userId
-            }
-        }]
+        where : {userId : req.userId}
+        // include : [{
+        //     model : User,
+        //     where : {
+        //         id : req.userId
+        //     }
+        // }]
     }).then(data=>{
         console.log(data);
         console.log('Successfully  Fetch all tweets');
@@ -52,6 +53,43 @@ exports.Tweets = async (req, res) =>{
         console.log('Some error while posting tweet',err.message);
         res.status(500).send({
             message : "Some Internal Error"
+        })
+    })
+}
+
+exports.feed = (req, res) =>{
+    //Users.findAll({ order: [['updatedAt', 'DESC']]}); // or ASC
+    Tweet.findAll({  order : [['SentOn', 'ASC']]}).then(data=>{
+        console.log(data);
+        res.status(200).send(data);
+    }).catch(err=>{
+        console.log("Some Error while fetching the Feed", err.message);
+        res.status(500).send({
+            message : "Some internal Error"
+        })
+    })
+}
+
+exports.like = async (req, res) =>{
+    
+    let username;
+    await User.findByPk(req.userId).then(user=>{
+        username = user.username;
+    })
+    LikeObj = {
+        username : username,
+        tweetId : req.params.id
+    }
+    console.log(LikeObj);
+    Like.create(LikeObj).then(data=>{
+        console.log(data);
+        res.status(201).send({
+            message : `${username} liked tweet No ${req.params.id}`
+        })
+    }).catch(err=>{
+        console.log('Some error while like the tweet',err.message);
+        res.status(500).send({
+            message : "Some internal Error"
         })
     })
 }
