3c873fe3982b9485d0a7df53b2a3f9970e733379
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-08-19 08:31:58 +0530

    Adding user following controller

---
+++ controller/following.controller.js
@@ -0,0 +1,124 @@
+const db = require('../model');
+const User = db.user;
+const Follower = db.follower;
+const Following = db.following;
+
+exports.follow = (req, res) =>{
+    
+    let username = req.query.username;
+    let toFollow;
+    let From;
+
+    User.findOne({
+        where : {
+            username : username
+        }
+    }).then( async user=>{
+        if(!user) return res.status(404).send({message : "User not found"});
+        if(user.id == req.userId) return res.status(400).send({message : "You Can't follow itself !"})
+        From = req.userId;
+        toFollow = user.id; 
+        let follower = {
+            followerId : toFollow,
+            userId : From,
+            followingId: From     
+        }
+        let following = {
+            followingId : From,
+            userId : toFollow ,
+            followerId : toFollow
+        }
+        await Follower.create(follower).then(data=>{
+            console.log('Successfully create follower')
+        })
+        await Following.create(following).then(data=>{
+            console.log('Successfully create following')
+        })
+        res.status(200).send({
+            message : `Following ${username}`
+        })
+
+    }).catch(err=>{
+        console.log('Some internal error',err.message);
+        res.status(500).send({
+            message: "Some internal error occured while following the user"
+        })
+    })
+
+
+
+}
+
+exports.unfollow = (req, res) =>{
+    
+    let username = req.query.username;
+    let followerId;
+    let followingId;
+
+    User.findOne({
+        where : {
+            username : username
+        }
+    }).then( async user=>{
+        if(!user) return res.status(404).send({message : "User not found"});
+        followerId = req.userId;
+        followingId = user.id 
+        
+        await Follower.destroy({
+            where : {
+                followerId : followerId   
+            }
+        }).then(data=>{
+            console.log('Successfully destroy follower')
+        })
+        await Following.destroy({
+            where : {
+                followingId : followingId  
+            }
+        }).then(data=>{
+            console.log('Successfully destroy following')
+        })
+        res.status(200).send({
+            message : `Unfollow ${username}`
+        })
+
+    }).catch(err=>{
+        console.log('Some internal error',err.message);
+        res.status(500).send({
+            message: "Some internal error occured while Unfollowing the user"
+        })
+    })
+
+
+
+}
+
+exports.GetAllFollowers = async (req, res) =>{
+
+    let userId = req.userId;
+    await Follower.findAll({
+        // attributes: ["userId"],
+        where : {followerId : userId},
+        include :[
+            {
+                model: User,
+                attributes: ["username", "email"]
+            }
+        ]
+    }).then(data=>{
+        let followers = [];
+        data.forEach(data =>{
+            followers.push(data.user);
+        })
+        // console.log(followers);
+        res.status(200).send({
+            followers : followers.length,
+            content : followers
+        });
+    }).catch(err=>{
+        console.log("Some Error while fetching all the followers", err.message);
+        res.status(500).send({
+            message : "Some interanal error"
+        })
+    })
+}
---
+++ controller/user.controller.js
@@ -0,0 +1,81 @@
+const db = require('../model');
+const User = db.user;
+const Follower = db.follower;
+const Following = db.following;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.config');
+
+exports.signUp = (req, res)=>{
+    const userObj = {
+        username : req.body.username,
+        email : req.body.email,
+        password : bcrypt.hashSync(req.body.password, 8)
+    }
+
+    User.create(userObj).then(user=>{
+        console.log("User registered SuccessFully ");
+        res.status(200).send({
+            message : " User registered SuccessFully "
+        });
+    }).catch(err=>{
+        console.log('Some while creating user', err);
+        res.status(500).send({
+            message : "Some internal Error"
+        })
+    })
+}
+
+exports.signIn = (req, res)=>{
+
+    User.findOne({
+        where : {
+            username : req.body.username
+        }
+    }).then(async user=>{
+        if(!user){
+            return res.status(404).send({message : "User not Found"});
+        }
+
+        var isValidPassword = bcrypt.compareSync(req.body.password, user.password);
+        if(!isValidPassword){
+            return res.status(401).send({
+                message : "Invalid Password"
+            })
+        }
+
+        var token = jwt.sign({ id : user.id }, authConfig.secret, {
+            expiresIn : 86400
+        })
+        var follower  
+        var following
+
+        await Follower.count({
+            where : {
+                followerId : user.id
+            }
+        }).then(data=>{
+            follower = data;
+
+        })
+        await Following.count({
+            where : {
+                followingId : user.id
+            }
+        }).then(data=>{
+            following = data;
+
+        })
+        console.log('---------------------')
+        res.status(200).send({
+            id : user.id,
+            username : user.username,
+            email : user.email,
+            Follower : follower,
+            Following : following,
+            accessToken : token
+        })
+    }).catch(err => {
+        res.status(500).send({ message: err.message });
+    })
+}
